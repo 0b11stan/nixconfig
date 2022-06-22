92fbeb744859ba323c42f8c08beb9eeae8f7ca2e
Tristan Pinaudeau <tristan@tic.sh>
2022-06-23 00:30:03 +0200

    fix entering passwords at start of upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
+eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa && ./sync-pass.sh
 ./update-system.sh && ./apply-system.sh
 ./update-user.sh && ./apply-user.sh
 ./nix-free-space.sh
-
-eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa && ./sync-pass.sh
