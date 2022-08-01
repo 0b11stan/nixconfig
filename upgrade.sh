7755583d50176c583eafd9720095807e98cb3b7a
Tristan Pinaudeau <tristan@tic.sh>
2022-08-01 21:47:08 +0200

    avoid full /boot

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa && ./sync-pass.sh
+sudo nix-collect-garbage -d
 ./update-system.sh && ./apply-system.sh
 ./update-user.sh && ./apply-users.sh
 ./nix-free-space.sh
