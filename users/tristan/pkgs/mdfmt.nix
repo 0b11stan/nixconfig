c220a4a4769e1d7cb5b9663b7c732d4c8bb97f1a
Tristan Pinaudeau <tristan@tic.sh>
2022-08-30 17:06:11 +0200

    fix mdfmt with github recommandations

--- users/tristan/pkgs/mdfmt.nix
+++ users/tristan/pkgs/mdfmt.nix
@@ -5,16 +5,16 @@
 }:
 buildGoModule rec {
   pname = "mdfmt";
-  version = "29e3d55";
+  version = "unstable-20181128";
 
   src = fetchFromGitHub {
     owner = "moorereason";
     repo = "mdfmt";
-    rev = "v${version}";
+    rev = "29e3d55cbe5af4cfc31b172aed1d69b9a5a22c5b";
     sha256 = "122k9rjm4g0cksgg1pywm3q64qhgvyq2275mrszjcp4ry15kpd74";
   };
 
-  vendorSha256 = "sha256-hNlVSelaU0DAfeRfz4qfXfmXeBIoWlf57S58jwE6/Zk=";
+  vendorSha256 = "sha256-vZeShukNP9EK82RsXEs1A4tji0NAGTINVgvTOnAhekA=";
 
   meta = with lib; {
     description = "Like gofmt, but for Markdown with front matter";
