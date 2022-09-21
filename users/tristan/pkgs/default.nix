f310bc33ac51ffde16eef046b96cffbccbeace9c
Tristan Pinaudeau <tristan@tic.sh>
2022-09-21 10:51:57 +0200

    update discord

--- users/tristan/pkgs/default.nix
+++ users/tristan/pkgs/default.nix
@@ -1,4 +1,12 @@
-self: super: {
+self: super: let
+  nixpkgs = ((import <nixpkgs>) {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "master";
+    sha256 = "sha256-MiGg5skTOc0a6Acp5tahoZCR1ymhP4zSLRv/fOxh9hY=";
+  };
+in {
+  discord = super.callPackage "${nixpkgs}/pkgs/applications/networking/instant-messengers/discord/default.nix" {};
   mdfmt = super.callPackage ./mdfmt.nix {}; # TODO : fix this package
   bloodhound = super.callPackage ./bloodhound/default.nix {}; # TODO : fix this package
   windapsearch = super.callPackage ./windapsearch.nix {};
