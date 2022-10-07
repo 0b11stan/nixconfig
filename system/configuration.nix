e81bd656aca262966868b4db8c09c38506a395f0
Tristan Pinaudeau <tristan@tic.sh>
2022-10-07 10:38:50 +0200

    open port 4444

--- system/configuration.nix
+++ system/configuration.nix
@@ -65,7 +65,7 @@
   services.pipewire.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [4444];
+  networking.firewall.allowedTCPPorts = [4444];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
