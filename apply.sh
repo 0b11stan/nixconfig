9f0c874a2761efc6db071e6b57bc458353b5c852
Tristan Pinaudeau <tristan@tic.sh>
2023-10-09 09:20:57 +0200

    forget after 15d

--- apply.sh
+++ apply.sh
@@ -3,4 +3,4 @@ eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa && pass git pull && pass git push
 sudo nix-channel --update
 sudo HOSTNAME=$HOSTNAME \
   nixos-rebuild switch -I nixos-config=./system/configuration.nix
-sudo nix-collect-garbage --delete-older-than 30d
+sudo nix-collect-garbage --delete-older-than 15d
