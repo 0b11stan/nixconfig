3271cb2b291515a756a9ee7bdd8c6ce2de9210ee
Tristan Pinaudeau <tristan@tic.sh>
2024-02-14 22:50:51 +0100

    delete old profiles and generations to save space

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa && pass git pull && pass git push
+nix-env --delete-generations +3
+sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +3
 ./apply.sh && sudo nix-collect-garbage --delete-older-than 15d
