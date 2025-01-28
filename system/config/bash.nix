49df672e7ea243dd7a1e43b6f56af820d8718650
Tristan Pinaudeau <tristan@tic.sh>
2025-01-28 11:38:20 +0100

    add aws helpers

--- system/config/bash.nix
+++ system/config/bash.nix
@@ -1,6 +1,19 @@
-{
+{pkgs}: {
   enable = true;
   bashrcExtra = ''
     cdx() { cd "$1"; ls -l; }; alias cd="cdx"
+
+    aws-login() {
+      export AWS_PROFILE=$(aws configure list-profiles | ${pkgs.fzf}/bin/fzf)
+      echo "Logging to $AWS_PROFILE"
+      aws sso login --profile "$AWS_PROFILE"
+    }
+
+    awssm() {
+      aws ssm start-session --target $(
+        aws ec2 describe-instances --filter Name=tag:Name,Values=$1 \
+        | jq -r '.Reservations[0].Instances[0].InstanceId'
+      );
+    }
   '';
 }
